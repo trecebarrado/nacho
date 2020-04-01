5326da548e90b5b1c805b4fcaa635e03877a796a
trecebarrado <trecebarrado@gmail.com>
2020-04-01 18:30:21 +0200

    conversor numeros naturales a romanos / romanos a naturales

---
+++ ejerciciosSueltos04numerosRomanos/main2.py
@@ -0,0 +1,74 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ventanas import ventana_principal
+import sys
+
+dicc = {0:"", 1:"", 2:"X", 3:"C", 4:"M", 50:"L", 500:"D", 100:"C", 1000:"M"}
+dicc_r = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+def conversor():    
+    num = ui.entrada_num.text()
+    if num[0] in ("I","V","X","L","C","D","M"):
+        ui.label_resultado.setText(conversor_rom(num))
+    else:
+        ui.label_resultado.setText(conversor_arab(num))
+        
+def conversor_rom(num):
+    long = len(num)
+    result = 0
+    for i in range(long-1):
+        if dicc_r[num[i]] >= dicc_r[num[i+1]]:
+            result += dicc_r[num[i]]
+        else:
+            result -= dicc_r[num[i]]
+    obtenido = result + dicc_r[num[long-1]]
+    comprobacion = conversor_arab(str(obtenido))
+    if num == comprobacion:
+        return str(obtenido)
+    else:
+        return "No es un nº válido"
+        
+def conversor_arab(num):
+    if int(num) == 0 or int(num) > 3999:
+        return "No es un nº válido"
+    else:
+        long = len(num)
+        result = ""
+        for i in range(long-1):
+            if int(num[i]) <= 3:
+                result += dicc[long] * int(num[i])
+                long -= 1
+            elif int(num[i]) == 4:
+                result += dicc[long] + dicc[int("5"+"0"*(long-1))]
+                long -= 1
+            elif int(num[i]) == 5:
+                result += dicc[int("5"+"0"*(long-1))]
+                long -= 1
+            elif int(num[i]) > 5 and int(num[i]) < 9:
+                result += dicc[int("5"+"0"*(long-1))] + (dicc[long] * (int(num[i])-5))
+                long -= 1
+            elif int(num[i]) == 9:
+                result += dicc[long] + dicc[int("1"+"0"*(long))]
+                long -= 1
+        return result + str(unidades(int(num[-1])))
+        
+def unidades(num):
+    if num <= 3:
+        return "I" * num
+    elif num == 4:
+        return "IV"
+    elif num == 9:
+        return "IX"
+    else:
+        return "V" + "I" * (num-5)
+    
+app = QtWidgets.QApplication(sys.argv)
+
+MainWindow = QtWidgets.QMainWindow()
+
+ui = ventana_principal.Ui_MainWindow()
+ui.setupUi(MainWindow)
+
+ui.btn_convertir.clicked.connect(conversor)
+
+MainWindow.show()
+sys.exit(app.exec_())
---
+++ ejerciciosSueltos04numerosRomanos/ventanas/ventana_principal.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ventana_principal.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(231, 233)
+        MainWindow.setStyleSheet("* {\n"
+"    background-image: url(\"marmol.jpg\");\n"
+"}")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_texto = QtWidgets.QLabel(self.centralwidget)
+        self.label_texto.setGeometry(QtCore.QRect(20, 0, 201, 61))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(8)
+        self.label_texto.setFont(font)
+        self.label_texto.setObjectName("label_texto")
+        self.label_resultado = QtWidgets.QLabel(self.centralwidget)
+        self.label_resultado.setGeometry(QtCore.QRect(20, 170, 191, 31))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(10)
+        self.label_resultado.setFont(font)
+        self.label_resultado.setStyleSheet("* {\n"
+"    border-style: inset;\n"
+"    border-width:5px;\n"
+"    border-color: rgb(170, 170, 255);\n"
+"}")
+        self.label_resultado.setFrameShape(QtWidgets.QFrame.WinPanel)
+        self.label_resultado.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.label_resultado.setText("")
+        self.label_resultado.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_resultado.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextSelectableByMouse)
+        self.label_resultado.setObjectName("label_resultado")
+        self.entrada_num = QtWidgets.QLineEdit(self.centralwidget)
+        self.entrada_num.setGeometry(QtCore.QRect(20, 60, 191, 31))
+        font = QtGui.QFont()
+        font.setFamily("Verdana")
+        font.setPointSize(10)
+        self.entrada_num.setFont(font)
+        self.entrada_num.setStyleSheet("* {\n"
+"    background-color: rgb(255, 255, 255);\n"
+"    border-style: inset;\n"
+"    border-width: 5px;\n"
+"    border-color: rgb(170, 170, 255);\n"
+"}")
+        self.entrada_num.setFrame(True)
+        self.entrada_num.setAlignment(QtCore.Qt.AlignCenter)
+        self.entrada_num.setObjectName("entrada_num")
+        self.btn_convertir = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_convertir.setGeometry(QtCore.QRect(20, 110, 191, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.btn_convertir.setFont(font)
+        self.btn_convertir.setStyleSheet("")
+        self.btn_convertir.setObjectName("btn_convertir")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "CONVERSOR"))
+        self.label_texto.setText(_translate("MainWindow", "Introduce un nº natural o un nº\n"
+"romano, menor de 4000:"))
+        self.btn_convertir.setText(_translate("MainWindow", "CONVERTIR"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
