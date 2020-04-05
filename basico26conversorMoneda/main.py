56f28c291e80f14852148e4dcd88c8cc61bf96fe
trecebarrado <trecebarrado@gmail.com>
2020-04-05 12:49:48 +0200

    conversor moneda

--- basico26conversorMoneda/main.py
+++ basico26conversorMoneda/main.py
@@ -2,41 +2,50 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ventanas import ventana_python
 import sys
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
 
-def transformar_a_dolares():
-    introducido= ui.entrada_euros.text()
-    introducido_float = float(introducido.replace(",","."))
-    dolares = round((introducido_float * 1.1), 2)
-    ui.label_resultado.setText(str(dolares).replace(".",",")+" Dólares")
-    
-def transformar_a_libras():
-    introducido= ui.entrada_euros.text()
-    introducido_float = float(introducido.replace(",","."))
-    libras = round((introducido_float * 0.9), 2)
-    ui.label_resultado.setText(str(libras).replace(".",",")+" Libras")
-    
-def transformar_a_yuanes():
-    introducido= ui.entrada_euros.text()
-    introducido_float = float(introducido.replace(",","."))
-    yuanes = round((introducido_float * 7.9), 2)
-    ui.label_resultado.setText(str(yuanes).replace(".",",")+" Yuanes")
 
-if __name__ == "__main__":   
-    #obligatorio para usar pyqt5:
+class Conversor_moneda(QMainWindow): 
+    def __init__(self):
+        super().__init__()     
+        self.conversor = ventana_python.Ui_MainWindow()
+        self.conversor.setupUi(self)
+        self.conversor.boton_convertir_a_dolares.clicked.connect(self.transformar_a_dolares)
+        self.conversor.boton_convertir_a_libras.clicked.connect(self.transformar_a_libras)
+        self.conversor.boton_convertir_a_yuanes.clicked.connect(self.transformar_a_yuanes)
+        self.show()
+        
+    def transformar_a_dolares(self):
+        introducido = self.conversor.entrada_euros.text()
+        try:
+            introducido_float = float(introducido.replace(",","."))
+            dolares = round((introducido_float * 1.1), 2)
+            self.conversor.label_resultado.setText(str(dolares).replace(".",",")+" Dólares")
+        except:
+            QMessageBox.about(MainWindow, "Info", "Introduce un nº       ")
+        
+    def transformar_a_libras(self):
+        introducido = self.conversor.entrada_euros.text()
+        try:
+            introducido_float = float(introducido.replace(",","."))
+            libras = round((introducido_float * 0.9), 2)
+            self.conversor.label_resultado.setText(str(libras).replace(".",",")+" Libras")
+        except:
+            QMessageBox.about(MainWindow, "Info", "Introduce un nº       ")
+               
+    def transformar_a_yuanes(self):
+        introducido = self.conversor.entrada_euros.text()
+        try:
+            introducido_float = float(introducido.replace(",","."))
+            yuanes = round((introducido_float * 7.9), 2)
+            self.conversor.label_resultado.setText(str(yuanes).replace(".",",")+" Yuanes")
+        except:
+            QMessageBox.about(MainWindow, "Info", "Introduce un nº       ")
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    #se prepara un MainWindow, es parte del protocolo
     MainWindow = QtWidgets.QMainWindow()
-    
-    #crea un objeto de la clase en el archivo generado para preparar la ventana principal (MainWindow)
-    #para que tenga todo el diseño que tenga en el Designer
     ui = ventana_python.Ui_MainWindow()
     ui.setupUi(MainWindow)
-    
-    #todos los widgets y componentes puestos en la ventana por qtdesigner estan en ui
-    ui.boton_convertir_a_dolares.clicked.connect(transformar_a_dolares)
-    ui.boton_convertir_a_libras.clicked.connect(transformar_a_libras)
-    ui.boton_convertir_a_yuanes.clicked.connect(transformar_a_yuanes)
-    
-    #se muestra la ventana principal de pyqt5
-    MainWindow.show()
+    convertir = Conversor_moneda()   
     sys.exit(app.exec_())
